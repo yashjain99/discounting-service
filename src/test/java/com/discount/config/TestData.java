bef4dc1f9a02fb6157a9e3f28faf4aaef221feef
yash-jain-sken <yash@salesken.ai>
2025-10-11 12:36:51 +0530

    Fixed initial claude code -

---
+++ src/test/java/com/discount/config/TestData.java
@@ -0,0 +1,17 @@
+package com.discount.config;
+
+import com.discount.model.CartItem;
+import com.discount.model.CustomerProfile;
+import com.discount.model.PaymentInfo;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class TestData {
+    private List<CartItem> cartItems;
+    private CustomerProfile customer;
+    private PaymentInfo paymentInfo;
+}
